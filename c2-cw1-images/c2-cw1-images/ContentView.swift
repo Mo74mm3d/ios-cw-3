22cac36524179cdce7630596e0e80e438408005e
Mo74mm3d <mohammedjalqattan@icloud.com>
2022-03-07 20:20:06 +0300

    Done

--- c2-cw1-images/c2-cw1-images/ContentView.swift
+++ c2-cw1-images/c2-cw1-images/ContentView.swift
@@ -9,32 +9,65 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-        Text("My Favorite Characters")
-            .fontWeight(.bold)
-            .font(.title)
-            .padding()
-        Spacer()
-                .frame(height:150)
-            HStack {
-        Image("Batman3")
-            .resizable()
-            .frame(width: 150, height: 250)
-        Image("spiderman")
+        ZStack{
+            Image("sky")
                 .resizable()
-                .frame(width:150, height: 150)
-            }
-            HStack {
-            Text("Batman")
-                .font(.title)
+                .scaledToFill().ignoresSafeArea()
+            VStack{
+            Text("My Favorite Characters")
                 .fontWeight(.bold)
-            Spacer()
-                .frame(width:35)
-            Text("Spiderman")
                 .font(.title)
-                .fontWeight(.bold)
-            }
+                .padding()
             Spacer()
+                    .frame(height:150)
+                HStack {
+            Image("Batman3")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 150)
+            Image("spiderman")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:100, height:100)
+                }
+                HStack{
+                    Text("Batman")
+                        .fontWeight(.heavy)
+                    Spacer()
+                        .frame(width:55)
+                Text("Spiderman")
+                        .fontWeight(.heavy)
+                }
+                HStack {
+                    Spacer()
+                Image("richtofen")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 150)
+                Image("superman")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 150)
+                Image("nedStark")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 150)
+                Spacer()
+                }
+                HStack{
+                    Text("Richtofen")
+                        .fontWeight(.heavy)
+                    Spacer()
+                        .frame(width:35)
+                    Text("Superman")
+                        .fontWeight(.heavy)
+                    Spacer()
+                        .frame(width:35)
+                    Text("Ned Stark")
+                        .fontWeight(.heavy)
+                }
+                Spacer()
+            }
         }
     }
 }
